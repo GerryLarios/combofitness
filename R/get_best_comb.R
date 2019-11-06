53111bb6b031cc45d1794fdf0ecee535d66f9503
Gerardo Manuel Chávez Larios <gerardo.larios69@gmail.com>
2019-11-06 10:28:24 -0600

    [REF] sorting by concept

--- R/get_best_comb.R
+++ R/get_best_comb.R
@@ -1,4 +1,4 @@
-get_best_comb <- function(result) {
-  ordered <- result[order(result$mse), ]
+get_best_comb <- function(result, concept = "mse") {
+  ordered <- result[order(result[concept]), ]
   return(as.list(ordered[1,]))
 }
